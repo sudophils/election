97528416123a0eb75f8e047d2e63a166cccff811
philips <philencripted@gmail.com>
2020-02-03 02:00:18 +0100

    add all

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+    // store candidate
+    string public candidate;
+    // read candidate
+    // constructor
+
+    constructor() public {
+        candidate = "candidate 1";
+    }
+
+}
